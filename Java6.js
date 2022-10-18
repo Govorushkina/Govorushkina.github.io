842a18b5765424707ca13ba1ce14414c26290403
Govorushkina <112868446+Govorushkina@users.noreply.github.com>
2022-10-18 20:57:48 +0300

    Update Java6.js

--- Java6.js
+++ Java6.js
@@ -1,15 +1,15 @@
 function onClick(){
 var kolvo=document.getElementById("kolvo").value;
 var stoim=document.getElementById("stoim").value;
-var result=document.getElementById("result");
+var resul=document.getElementById("resul");
 var a;
 var b;
- result.innerHTML = "";
+ resul.innerHTML = "";
 a = kolvo;
 b = stoim;
 if (a>0 && a!=NaN && b>0 && b!=NaN)
-result.innerHTML=a*b;    
-else result.innerText="Данные введены неверно";    
+resul.innerHTML=a*b;    
+else resul.innerText="Данные введены неверно";    
 }
 window.document.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
