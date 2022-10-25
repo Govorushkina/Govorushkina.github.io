c523a1e9586a2822d731b95059efac8ee554ad6c
Govorushkina <112868446+Govorushkina@users.noreply.github.com>
2022-10-25 16:02:40 +0300

    Update Java7.js

--- Java7.js
+++ Java7.js
@@ -1,1 +1,24 @@
+$(document).ready(function() {
+  $(".pictures").slick({
+    slidesToShow:4,
+     slidesToScrol:1,
+    dots: true,
+    responsive:[
+      {
+        breakpoint: 1024,
+        settings:{
+          slidesToShow:3,
+     slidesToScrol:1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings:{
+          slidesToShow:2,
+     slidesToScrol:1,
+        },
+      },
+      ],
+  });
+});
 
